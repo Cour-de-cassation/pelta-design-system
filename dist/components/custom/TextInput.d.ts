d68c2d9378293abbb08bff290b906541a5cca0dc
David Libeau <David.Libeau@justice.fr>
2023-04-18 10:46:18 +0200

    Fix textInput

--- dist/components/custom/TextInput.d.ts
+++ dist/components/custom/TextInput.d.ts
@@ -3,6 +3,7 @@ export { TextInput };
 declare function TextInput(props: {
     name: string;
     placeholder?: string;
+    alignRight?: boolean;
     value: string;
     onChange: (value: string) => void;
 }): ReactElement;
