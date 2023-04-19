a2cca18d9e01f2d3416739cf80014695a7c1ad64
David Libeau <David.Libeau@justice.fr>
2023-04-19 10:16:36 +0200

    Fix

--- dist/components/custom/TextInput.js
+++ dist/components/custom/TextInput.js
@@ -19,5 +19,6 @@ exports.TextInput = TextInput;
 function buildStyles(theme, alignRight) {
     return {
         input: Object.assign({ width: "100%", boxSizing: "border-box", padding: theme.spacing, backgroundColor: "transparent", border: "none", color: theme.colors.line.level1, borderBottom: `${theme.colors.line.level2} 2px solid`, textAlign: "right" }, theme_1.typography.body1.normal),
+    };
 }
 //# sourceMappingURL=TextInput.js.map
